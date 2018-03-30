0464af29cbd160478987c227dc7e9d1b6574b50a
Ryan Calder <ryan@jgquinn.com>
2018-03-30 15:52:28 -0500

    add initial utilities

---
+++ coozy.go
@@ -0,0 +1,50 @@
+package coozy
+
+import (
+	"github.com/gobuffalo/pop"
+)
+
+// Version contains the package version.
+const Version = "0.0.0"
+
+// WhereLiker defines the WhereLike() method used by FindPop.
+type WhereLiker interface {
+	WhereLike(q *pop.Query) *pop.Query
+}
+
+// SavePop saves a record struct into the named environment.
+func SavePop(popEnvironmentName string, rec interface{}) (err error) {
+
+	var tx *pop.Connection
+	tx, err = pop.Connect(popEnvironmentName)
+	if err != nil {
+		return
+	}
+
+	err = tx.Save(rec)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// FindPop queries the named environment for records based on the supplied criteria.
+func FindPop(popEnvironmentName string, criteria WhereLiker, recs interface{}, eagerFetchFields ...string) (err error) {
+
+	var tx *pop.Connection
+	tx, err = pop.Connect(popEnvironmentName)
+	if err != nil {
+		return
+	}
+
+	var q *pop.Query
+	if len(eagerFetchFields) > 0 {
+		q = tx.Eager(eagerFetchFields...)
+	} else {
+		q = tx.Q()
+	}
+
+	err = criteria.WhereLike(q).All(recs)
+	return
+}
---
+++ queries.go
@@ -0,0 +1,53 @@
+package coozy
+
+import (
+	"strings"
+
+	"github.com/gobuffalo/pop/nulls"
+	"github.com/gobuffalo/uuid"
+)
+
+// AppendQueryUUIDCriteria adds an AND clause to the query being constructed in with the provided Builder and parameter values slice.
+func AppendQueryUUIDCriteria(queryBuilder *strings.Builder, queryParameters *[]interface{}, fieldName string, fieldValue uuid.UUID) (added bool) {
+	if fieldValue != uuid.Nil {
+		if queryBuilder.Len() > 0 {
+			queryBuilder.WriteString(" AND ")
+		}
+		queryBuilder.WriteString(fieldName)
+		queryBuilder.WriteString(" = ?")
+		*queryParameters = append(*queryParameters, fieldValue)
+		added = true
+	}
+
+	return
+}
+
+// AppendQueryStringCriteria adds an AND clause to the query being constructed in with the provided Builder and parameter values slice.
+func AppendQueryStringCriteria(queryBuilder *strings.Builder, queryParameters *[]interface{}, fieldName string, fieldValue string) (added bool) {
+	if fieldValue != "" {
+		if queryBuilder.Len() > 0 {
+			queryBuilder.WriteString(" AND ")
+		}
+		queryBuilder.WriteString(fieldName)
+		queryBuilder.WriteString(" = ?")
+		*queryParameters = append(*queryParameters, fieldValue)
+		added = true
+	}
+
+	return
+}
+
+// AppendQueryNullableStringCriteria adds an AND clause to the query being constructed in with the provided Builder and parameter values slice.
+func AppendQueryNullableStringCriteria(queryBuilder *strings.Builder, queryParameters *[]interface{}, fieldName string, fieldValue nulls.String) (added bool) {
+	if fieldValue.Valid {
+		if queryBuilder.Len() > 0 {
+			queryBuilder.WriteString(" AND ")
+		}
+		queryBuilder.WriteString(fieldName)
+		queryBuilder.WriteString(" = ?")
+		*queryParameters = append(*queryParameters, fieldValue)
+		added = true
+	}
+
+	return
+}
