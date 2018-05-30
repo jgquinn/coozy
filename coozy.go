04091279a4c479aa5091634b88526db9d826bf41
Ryan Calder <ryan@jgquinn.com>
2018-05-30 11:47:13 -0500

    increment version number

--- coozy.go
+++ coozy.go
@@ -8,7 +8,7 @@ import (
 )
 
 // Version contains the package version.
-const Version = "0.0.4-pre"
+const Version = "0.0.4"
 
 // WhereLiker defines the WhereLike() method used by FindPop.
 type WhereLiker interface {
